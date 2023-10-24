1a38dedd6504333ea3c09456992f9b4a91c5e217
bp05 <bastian.parada05@inacapmail.cl>
2023-10-23 22:57:46 -0300

    splash avanzado

--- app/src/main/java/com/example/cajeros/Splash.kt
+++ app/src/main/java/com/example/cajeros/Splash.kt
@@ -1,11 +1,17 @@
 package com.example.cajeros
 
 
+import android.content.Context
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.util.Log
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
 
 
 class Splash : AppCompatActivity() {
@@ -15,8 +21,46 @@ class Splash : AppCompatActivity() {
 
         Handler(Looper.getMainLooper()).postDelayed({
             val intent = Intent(this, MainActivity::class.java)
-            startActivity(intent)
-            finish()
+
+            if (isOnline(this)){
+                startActivity(intent)
+                finish()
+            }else{
+                Toast.makeText(this@Splash, "No hay conexion a internet", Toast.LENGTH_SHORT).show()
+                val alertbuilder = AlertDialog.Builder(this@Splash)
+                alertbuilder.setTitle("No hay internet")
+                alertbuilder.setMessage("Por favor, activa el internet")
+                alertbuilder.setPositiveButton(
+                    "OK"
+                ) { dialogInterface, i -> finish() }
+                val dialog = alertbuilder.show()
+                dialog.setCancelable(false)
+                dialog.setCanceledOnTouchOutside(false)
+            }
+
+
         }, 3000)
+    }
+
+    fun isOnline(context: Context): Boolean {
+        val connectivityManager =
+            context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        if (connectivityManager != null) {
+            val capabilities =
+                connectivityManager.getNetworkCapabilities(connectivityManager.activeNetwork)
+            if (capabilities != null) {
+                if (capabilities.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR)) {
+                    Log.i("Internet", "NetworkCapabilities.TRANSPORT_CELLULAR")
+                    return true
+                } else if (capabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI)) {
+                    Log.i("Internet", "NetworkCapabilities.TRANSPORT_WIFI")
+                    return true
+                } else if (capabilities.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET)) {
+                    Log.i("Internet", "NetworkCapabilities.TRANSPORT_ETHERNET")
+                    return true
+                }
+            }
+        }
+        return false
     }
 }
