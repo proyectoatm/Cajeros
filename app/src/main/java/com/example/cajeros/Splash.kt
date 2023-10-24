7e3828e18ab77ad28029b485e9c26906025cc88f
bp05 <bastian.parada05@inacapmail.cl>
2023-10-23 22:21:47 -0300

    testeo de logo en splash

--- app/src/main/java/com/example/cajeros/Splash.kt
+++ app/src/main/java/com/example/cajeros/Splash.kt
@@ -1,11 +1,15 @@
 package com.example.cajeros
 
+
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+
 
 class Splash : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+
     }
 }
