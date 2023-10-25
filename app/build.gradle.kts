430ee0ec7422d7737300b124d08ad4210a955015
bp05 <bastian.parada05@inacapmail.cl>
2023-10-25 17:51:58 -0300

    prototipo de login 1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("com.google.gms.google-services")
+
 }
 
 android {
@@ -37,11 +39,16 @@ android {
 
 dependencies {
 
+    implementation(platform("com.google.firebase:firebase-bom:32.4.0"))
+    implementation("com.google.firebase:firebase-analytics-ktx")
+
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("com.google.firebase:firebase-auth:22.2.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
 }
--- app/src/main/java/com/example/cajeros/Login.kt
+++ app/src/main/java/com/example/cajeros/Login.kt
@@ -1,11 +1,81 @@
 package com.example.cajeros
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class Login : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        lateinit var auth: FirebaseAuth
+        // Initialize Firebase Auth
+        auth = Firebase.auth
+        setup(auth)
+
+
     }
+
+
+    public override fun onStart() {
+        super.onStart()
+        // Check if user is signed in (non-null) and update UI accordingly.
+        val currentUser = Firebase.auth.currentUser
+        if (currentUser != null) {
+            Toast.makeText(this@Login, "si se ha accedido, con: "+currentUser.email.toString(), Toast.LENGTH_SHORT).show()
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+            //reload()
+        }else{
+            Toast.makeText(this@Login, "Debes acceder", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun setup(auth: FirebaseAuth) {
+        title="Autenticacion"
+
+        val boton_registro = findViewById<View>(R.id.boton_registro) as Button
+        val email = findViewById<View>(R.id.email) as EditText
+        val clave = findViewById<View>(R.id.clave) as EditText
+        val claverep = findViewById<View>(R.id.repetir_clave) as EditText
+        boton_registro.setOnClickListener {
+            if (email.text.isNotEmpty() && clave.text.isNotEmpty() && claverep.text.isNotEmpty()){
+                Toast.makeText(this@Login, "primer if pasado", Toast.LENGTH_SHORT).show()
+                if (clave.text.toString() == claverep.text.toString()){
+                    Toast.makeText(this@Login, "segundo if pasado", Toast.LENGTH_SHORT).show()
+                    auth.createUserWithEmailAndPassword(email.text.toString(), clave.text.toString()).addOnCompleteListener(this){
+                        if (it.isSuccessful){
+                            val intent = Intent(this, MainActivity::class.java)
+                            startActivity(intent)
+                        }else{
+                            showAlert()
+                        }
+                    }
+                }
+            }
+        }
+    }
+    private fun showAlert() {
+        val alertbuilder = AlertDialog.Builder(this@Login)
+        alertbuilder.setTitle("Error")
+        alertbuilder.setMessage("Error al autenticar usuario")
+        alertbuilder.setPositiveButton(
+            "OK"
+        ) { dialogInterface, i -> finish() }
+        val dialog = alertbuilder.show()
+        dialog.setCancelable(false)
+        dialog.setCanceledOnTouchOutside(false)
+    }
+
+
 }
